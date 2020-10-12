facee233d21eba591e61f6b51041d7358f853046
Patrick <patrickcooper1995@gmail.com>
2020-10-12 19:33:06 -0400

    101220 - more alien progress

--- boxFunction.js
+++ boxFunction.js
@@ -3,36 +3,36 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function aliens(ctx, moveTimes, head_y, head_x) {
+async function aliens(ctx, moveTimes, antennaX, antennaY) {
 
 	for (i = 0; i < moveTimes; i++) {
 
 		ctx.fillStyle = "#32CD32";
 
 		// draw rest of alien's body
-		var left_y = head_y + 10;
-		var left_x = head_x - 10;
-		var right_y = head_y + 10;
-		var right_x = head_x + 10;
+    ctx.fillRect(antennaX, antennaY, 5, 5);
+    ctx.fillRect(antennaX + 5, antennaY + 5, 5, 5);
+    ctx.fillRect(antennaX, antennaY + 10, 40, 5);
+    ctx.fillRect(antennaX + 35, antennaY, 5, 5);
+    ctx.fillRect(antennaX + 30, antennaY + 5, 5, 5);
+    ctx.fillRect(antennaX - 5, antennaY + 15, 50, 5);
+    ctx.clearRect(antennaX + 5, antennaY + 15, 5, 5);
+    ctx.clearRect(antennaX + 30, antennaY + 15, 5, 5);
+    ctx.fillRect(antennaX - 10, antennaY + 20, 60, 5);
+    ctx.fillRect(antennaX - 10, antennaY + 25, 5, 10);
+    ctx.fillRect(antennaX, antennaY + 25, 40, 5);
+    ctx.fillRect(antennaX + 45, antennaY + 25, 5, 10);
+    ctx.fillRect(antennaX, antennaY + 30, 5, 5);
+    ctx.fillRect(antennaX + 35, antennaY + 30, 5, 5);
+    ctx.fillRect(antennaX + 5, antennaY + 35, 10, 5);
+    ctx.fillRect(antennaX + 25, antennaY + 35, 10, 5);
 
-		console.log(i);
-
-		// draw alien
-		ctx.fillRect(head_x, head_y, 10, 10);
-		ctx.fillRect(left_x, left_y, 10, 10);
-		ctx.fillRect(right_x, right_y, 10, 10);
-		console.log(head_x + ", " + head_y)
+		console.log(antennaX + ", " + antennaY);
 
 		await new Promise(resolve => setTimeout(resolve, 2000));
 
-		ctx.clearRect(head_x, head_y, 10, 10);
-		ctx.clearRect(left_x, left_y, 10, 10);
-		ctx.clearRect(right_x, right_y, 10, 10);
-
 		// prepare his next move
-		head_x += 30;
-		left_x += 30;
-		right_x += 30;
+		antennaX += 30;
 	}
 }
 
@@ -53,23 +53,37 @@ var moveRate = 30;
 var moveTimes = (width - 10) / moveRate;
 var positions = Math.floor((y_boundary - 10) / moveRate);
 
-// for (i = 0; i < 10; i++) {
-// 	yPosition = 100;
-//
-// 	aliens(ctx, moveTimes, yPosition, 10);
-// 	yPosition += 40;
-// }
+
 moveTimes = 290;
-aliens(ctx, moveTimes, 100, 10);
-aliens(ctx, moveTimes, 140, 10);
-aliens(ctx, moveTimes, 180, 10);
-aliens(ctx, moveTimes, 220, 10);
-aliens(ctx, moveTimes, 260, 10);
-aliens(ctx, moveTimes, 300, 10);
-aliens(ctx, moveTimes, 340, 10);
-aliens(ctx, moveTimes, 380, 10);
-aliens(ctx, moveTimes, 420, 10);
-aliens(ctx, moveTimes, 460, 10);
+
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, 1000, 1000);
+
+// Space Invaders alien
+ctx.fillStyle = "#32CD32";
+var antennaX = 150;
+var antennaY = 100;
+
+// Empire State Building
+ctx.fillRect(460, 750, 80, 250);
+ctx.fillRect(470, 710, 60, 40);
+ctx.fillRect(480, 690, 40, 20);
+ctx.fillRect(490, 685, 20, 5);
+ctx.fillRect(497, 645, 5, 40);
+ctx.fillRect(450, 925, 100, 75);
+ctx.clearRect(470, 750, 1, 250);
+ctx.clearRect(530, 750, 1, 250);
+
+aliens(ctx, moveTimes, 10, 100);
+// aliens(ctx, moveTimes, 140, 10);
+// aliens(ctx, moveTimes, 180, 10);
+// aliens(ctx, moveTimes, 220, 10);
+// aliens(ctx, moveTimes, 260, 10);
+// aliens(ctx, moveTimes, 300, 10);
+// aliens(ctx, moveTimes, 340, 10);
+// aliens(ctx, moveTimes, 380, 10);
+// aliens(ctx, moveTimes, 420, 10);
+// aliens(ctx, moveTimes, 460, 10);
 
 // function army() {
 // 	var canvas = document.getElementById("myCanvas");
